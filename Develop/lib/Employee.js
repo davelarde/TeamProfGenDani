5f07edc13f94f14ef10e85f5e10d7ecf0babf885
danivelarde <danielavelarde4@gmail.com>
2022-04-24 19:45:08 -0400

    adding classes

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,28 @@
+// TODO: Write code to define and export the Employee class
+class Employee{
+    constructor(name,id,email){
+this.name =name;
+this.id =id;
+this.email= email;
+    }
+    getName(){
+        return this.name
+    }
+getId(){
+    return this.id
+}
+getEmail(){
+    return this.email
+}
+getRole(){
+    return 'Employee';
+}
+}
+// const newName= new Employee("Daniela");
+// newName.getName()
+// const newId = new Employee(4444);
+// newId.getId()
+// const newEmail = new Employee("dani@dani");
+// newEmail.getEmail()
+
+module.exports = Employee;
